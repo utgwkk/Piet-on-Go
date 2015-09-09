378b086ea6170cf4327c55870ad85404b9b32a03
UTAGAWA Kiki <utagawakiki@gmail.com>
2015-09-09 14:05:20 +0900

    add test

---
+++ src/main/piet_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func TestAll(t *testing.T) {
+  var piet Piet
+  var testlist = [][2]string{
+    {"../../tests/alpha_filled.png", "abcdefghijklmnopqrstuvwxyz"},
+    {"../../tests/Piet-1.gif", "Piet"}}
+
+  for _, tc := range testlist {
+    piet.New(tc[0])
+    piet.debug = false
+    piet.Run()
+    actual := piet.output
+    expected := tc[1]
+    if actual != expected {
+      t.Errorf("%v (actual) != %v (expected)", actual, expected)
+    }
+  }
+}
